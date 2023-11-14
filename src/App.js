ef15060490f73f05efdd70d2bab86909ccd042f2
anees1234567 <aneeshassainar786@gmail.com>
2023-11-14 22:15:53 +0530

    commit fro the first branch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <h2>this is the social media app  am trying to develop using react </h2>
+      
     </div>
   );
 }
