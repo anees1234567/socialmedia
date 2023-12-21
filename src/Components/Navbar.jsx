fc81db00cf9f479fa2c1832d35dabc59e1f52be1
anees1234567 <aneeshassainar786@gmail.com>
2023-12-21 22:21:22 +0530

    90% of navabar completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,35 +1,52 @@
-import { AppBar, Toolbar, Typography, styled } from '@mui/material';
+import { AppBar, Toolbar, Typography, styled,InputBase,Badge } from '@mui/material';
 import React from 'react';
-import Pets from '@mui/icons-material/Pets';
+import {Mail, Notifications, Pets} from '@mui/icons-material';
 import * as Mui from '@mui/material'
 const StyledToolbar = styled(Mui.Toolbar)({
   display: 'flex',
   justifyContent: 'space-between',
   color:'white',
-  background:"palette.background.default"
+ 
 });
 
 
-const Search = styled('div')(({theme})=>({
-  backgroundColor:"white",
-  padding:"0 10px",
-  borderRadius:theme.shape.borderRadius,
+const Search = styled(InputBase)(({ theme }) => ({
+  background: 'white',
+  padding: '0 10px',
+  borderRadius: theme.shape.borderRadius,
+  color: 'black',
+  width: '40%',
 }));
 
 const Icons = styled('Box')(({theme})=>({
-  backgroundColor:"white"
+  
+    display:"flex",
+    gap:"20px",
+    alignItems:"center"
+
 }));
 
 function Navbar() {
   return (
     <AppBar position='sticky'>
       <StyledToolbar>
-        <Typography sx={{display:{xs:'none',sm:'block'}}}>Navbar</Typography>
-        <Pets sx={{display:{xs:'block',sm:'none'}}}/>
-        <Search>search</Search>
-        <Icons>icons</Icons>
+        <Typography sx={{ display: { xs: 'none', sm: 'block' } }}>Navbar</Typography>
+        <Pets sx={{ display: { xs: 'block', sm: 'none' } }} />
+        <Search placeholder='Search Here'/>
+        <Icons>
 
         
+
+          <Badge badgeContent={4} color="error">
+          <Mail color="white" />
+          </Badge>
+
+          <Badge badgeContent={4} color="error">
+          <Notifications color="white" />
+          </Badge>
+            <Mui.Avatar alt="Remy Sharp" src='/assets/person/1.jpeg' />
+
+        </Icons>
       </StyledToolbar>
     </AppBar>
   );
