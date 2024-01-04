74b70064756ba1c8a0618920fc08f9bec156ef40
Anees <anees@pappyjoe.com>
2024-01-04 17:49:10 +0530

    commit done on january4

--- src/Routes/Routerlist.jsx
+++ src/Routes/Routerlist.jsx
@@ -1,15 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import { RouterListTwo, RouterListone } from "./Mainrouterlist";
+import HomeLayoutContainer from "../layouts/HomeLayoutContainer";
+import SecondoryLayoutContainer from "../layouts/SecondoryLayoutContainer";
 
 
 
 
 
-export const mainRouter = createBrowserRouter([
+
+export const MainRouter = createBrowserRouter([
   {
     path:RouterListone.home ,
     element: <HomeLayoutContainer />,
  
+  },
+  {
+    path:RouterListTwo.root,
+    element: <SecondoryLayoutContainer />,
+    children:[
+        {
+          path:"friends",
+          
+        }
+    ]
   }
 
 
