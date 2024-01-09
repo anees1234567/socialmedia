fa2e9326bedf79e6ef51b32468e89d9a6545da18
anees1234567 <aneeshassainar786@gmail.com>
2024-01-09 23:26:22 +0530

    commit done on january 09

--- src/Components/common/leftside/Sidebar.jsx
+++ src/Components/common/leftside/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { Box, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-import {Mail,Feed, AddBox, Settings, Home, AccountCircle, Person, Group} from '@mui/icons-material';
+import {Mail,Feed, Settings, Home, AccountCircle, Person, Group} from '@mui/icons-material';
 import React from 'react';
 import { useNavigate } from 'react-router';
 
@@ -14,13 +14,12 @@ function Sidebar() {
 
   const options = [
     { text: "Home", icon: <Home />, path: '/' },
-    { text: "Inbox", icon: <Mail />, path: 'primary/inbox' },
-    { text: "Feeds", icon: <Feed />, path: 'primary/feeds' },
-    { text: "Create", icon: <AddBox />, path: 'primary/create' },
-    { text: "Friends", icon: <Person />, path: 'primary/friends' },
-    { text: "Groups", icon: <Group />, path: 'primary/groups' },
-    { text: "Settings", icon: <Settings />, path: 'primary/settings' },
-    { text: "Profile", icon: <AccountCircle />, path: 'primary/profile' },
+    { text: "Inbox", icon: <Mail />, path: '/inbox' },
+    { text: "Feeds", icon: <Feed />, path: '/feeds' },
+    { text: "Friends", icon: <Person />, path: 'friends' },
+    { text: "Groups", icon: <Group />, path: '/groups' },
+    { text: "Settings", icon: <Settings />, path: '/settings' },
+    { text: "Profile", icon: <AccountCircle />, path: '/profile' },
   ];
 
   
