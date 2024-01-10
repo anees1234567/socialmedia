4e11448671ed55674d0e5f77957c5ca1c1b50bf9
anees1234567 <aneeshassainar786@gmail.com>
2024-01-10 23:52:30 +0530

    commmit done on jan10 2024

---
+++ src/Components/SecondoryLayoutComponents/inbox/Chatbox.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+function Chatbox() {
+  return (
+    <Box  flex="8"  sx={{backgroundColor:'royalblue'}}>Chatbox</Box>
+  )
+}
+
+export default Chatbox
--- src/Components/SecondoryLayoutComponents/inbox/Inboxpage.jsx
+++ src/Components/SecondoryLayoutComponents/inbox/Inboxpage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Chatlist from './Chatlist'
+import Chatbox from './Chatbox'
+import { Stack } from '@mui/material'
 
 function Inboxpage() {
   return (
-    <div>Inboxpage</div>
+    <Stack  direction={'row'} style={{backgroundColor:'black',height:'80vh',width:'100vw'}}>
+
+
+        <Chatlist/>
+        <Chatbox/>
+
+
+
+    </Stack>
   )
 }
 
